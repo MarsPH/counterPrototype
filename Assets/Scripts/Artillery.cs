fab501c25101794abc2d9204fee3d7ffe7a65493
MarsPH <m.poorhamidian@gmail.com>
2024-05-08 13:50:35 -0400

    Changed Artillery.cs: The base Artillery function is now working. / ArtilleryBullet.cs to be done

--- Assets/Scripts/Artillery.cs
+++ Assets/Scripts/Artillery.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Artillery : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float heatingCap;
+    public float cooldown;
+    public float range;
+    private float shotAmount;
 
-    // Update is called once per frame
+    public Transform artillery;
+    public GameObject bulletPrefab;
+    public Camera cam;
+    private bool isCoolingDown;
+
+
+
     void Update()
     {
-        
+
+        if (shotAmount < heatingCap)
+        {
+            if (Input.GetKey(KeyCode.Space) && !isCoolingDown)
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (!isCoolingDown)
+            {
+                StartCoroutine(CoolDown());
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        float maxRayDistance = range;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * maxRayDistance, Color.yellow, 2f);
+
+        Vector3 direction = ray.direction;
+
+        GameObject bullet = Instantiate(bulletPrefab, artillery.position, Quaternion.LookRotation(direction));
+        //bullet.GetComponent<Rigidbody>().AddForce(direction * bullet.GetComponent<ArtilleryBullet>().speed);
+        shotAmount += 1;
+    }
+    IEnumerator CoolDown()
+    {
+        isCoolingDown = true;
+        yield return new WaitForSeconds(cooldown);
+        shotAmount = 0;
+        isCoolingDown = false;
     }
 }
