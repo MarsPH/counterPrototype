727868a2577d664bbc17d1bae4ad91f716602a3e
MarsPH <m.poorhamidian@gmail.com>
2024-05-09 23:52:01 -0400

    Fixed Bug: Minor Completion Error Occured due to missplacement of symbols / Added: a structure for ArtilleryBullet.cs

--- Assets/Scripts/ArtilleryBullet.cs
+++ Assets/Scripts/ArtilleryBullet.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class ArtilleryBullet : MonoBehaviour
 {
+    /// <summary>
+    /// Its going to be something like the bullet.
+    /// </summary>
     void Start()
     {
-        
+
+
     }
 
-]    void Update()
+    void Update()
     {
-        
+        // - Move bullet forward.
+        // - After a certain time or distance, check for nearby enemies.
+        // - If an enemy is close or time is up, explode.
+        // - Apply gravity effect to simulate curve due to gravity.
+
+
     }
+    // OnTriggerEnter or Physics.OverlapSphere for detecting proximity.
+    // Use Rigidbody for physics-based movement and applying forces.
 }
