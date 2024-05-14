5672d0cfb2257ae3688c3b008087202c8b9471e1
MarsPH <m.poorhamidian@gmail.com>
2024-05-13 22:41:01 -0400

    Changed: ArtierllyBullet.cs should now detect, or explode after a certain travel time(Effective Range)

--- Assets/Scripts/ArtilleryBullet.cs
+++ Assets/Scripts/ArtilleryBullet.cs
@@ -18,18 +18,48 @@ public class ArtilleryBullet : MonoBehaviour
     // - Instantiate: To create explosion effects.
     /// </summary>
     public float speed = 1.0f;
+    public float explosionRadius;
+    public float maxDestination = 100f;
     private Rigidbody rb;
+    private Vector3 firePosition;
+    private Vector3 currentPosition;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        firePosition = transform.position;
+        currentPosition = transform.position;
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        // - Move bullet forward.
-        // - After a certain time or distance, check for nearby enemies.
+        currentPosition = transform.position;
+        float travelDistance = Vector3.Distance(currentPosition, firePosition);
+
+        if (travelDistance < maxDestination)
+        {
+            rb.AddForce(Vector3.forward * speed, ForceMode.Acceleration);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, explosionRadius);
+        if (hitColliders.Length > 0)
+        {
+            foreach (Collider collider in hitColliders)
+            {
+                if (collider.gameObject.CompareTag("Enemy"))
+                {
+                    IncomingRocket incomingRocket = collider.gameObject.GetComponent<IncomingRocket>();
+                    incomingRocket.TakeDamage();
+                }
+            }
+        }
+    
         // - If an enemy is close or time is up, explode.
         // - Apply gravity effect to simulate curve due to gravity.
 
